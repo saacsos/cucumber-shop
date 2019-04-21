e0f0ced4341c9e43bd9c6f76fb210dd8b460f580
Saac Sornchai <saacsos@gmail.com>
2019-04-21 17:20:16 +0700

    Add product stocking

--- src/main/java/ku/shop/Product.java
+++ src/main/java/ku/shop/Product.java
@@ -3,10 +3,11 @@ package ku.shop;
 public class Product {
     private double price;
     private String name;
+    private int quantity;
 
     public Product(String name, double price) {
         this.name = name;
-        this.price = price;
+        setPrice(price);
     }
 
     public String getName() {
@@ -16,6 +17,10 @@ public class Product {
         return price;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -23,5 +28,17 @@ public class Product {
         if (price < 0)
             throw new IllegalArgumentException("Price must be not be negative zero");
         this.price = price;
+    }
+
+    public void stock(int quantity) {
+        if (quantity < 0)
+            throw new IllegalArgumentException("Stock number must be positive");
+        this.quantity += quantity;
+    }
+
+    public void pick(int quantity) {
+        if (quantity > this.quantity)
+            throw new IllegalArgumentException("Insufficient product quantity");
+        this.quantity -= quantity;
     }
 }
--- src/main/java/ku/shop/ProductCatalog.java
+++ src/main/java/ku/shop/ProductCatalog.java
@@ -11,11 +11,19 @@ public class ProductCatalog {
         products = new HashMap<>();
     }
 
-    public void addProduct(String name, double price) {
-        products.put(name, new Product(name, price));
+    public void addProduct(String name, double price, int quantity) {
+        Product product = new Product(name, price);
+        product.stock(quantity);
+        products.put(name, product);
     }
 
     public Product getProduct(String name) {
         return products.get(name);
+    }
+
+    public Product getProduct(String name, int quantity) {
+        Product product = products.get(name);
+        product.pick(quantity);
+        return product;
     }
 }
